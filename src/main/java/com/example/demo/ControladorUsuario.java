199daebaa6d348f57019010fecdafd97168d663b
Ray Silva <onikageriki@gmail.com>
2019-09-11 22:38:37 -0400

    Agregando linkto al ControladorUsuario

--- src/main/java/com/example/demo/ControladorUsuario.java
+++ src/main/java/com/example/demo/ControladorUsuario.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import java.util.List;
 
+import org.aspectj.weaver.Iterators;
+import org.springframework.hateoas.Resource;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 class ControladorUsuario {
@@ -31,13 +36,24 @@ class ControladorUsuario {
     }
 
     // item unico
-    @GetMapping("/usuarios/{id}")
+    /*@GetMapping("/usuarios/{id}")
     Usuario oneUsuario(@PathVariable Long id) {
 
         return repositorio.findById(id)
                 .orElseThrow(() -> new ExcepcionUsuarioNoEncontrado(id));
+    }*/
+    @GetMapping("/usuarios/{id}")
+    Resource<Usuario> one(@PathVariable Long id) {
+
+        Usuario usuario = repositorio.findById(id)
+                .orElseThrow(() -> new ExcepcionUsuarioNoEncontrado(id));
+
+        return new Resource<>(usuario,
+                linkTo(methodOn(ControladorUsuario.class).one(id)).withSelfRel(),
+                linkTo(methodOn(ControladorUsuario.class).all()).withRel("usuarios"));
     }
-        @PutMapping("/usuarios/{id}")
+
+    @PutMapping("/usuarios/{id}")
         Usuario replaceUsuario(@RequestBody Usuario nuevoUsuario, @PathVariable Long id) {
 
             return repositorio.findById(id)
