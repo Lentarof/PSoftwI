87c7a354b0126e5b1b74141adcad0bde32812645
Ray Silva <onikageriki@gmail.com>
2019-09-12 02:37:13 -0400

    Mejorando la Clase Usuario del directorio evolucion

--- evolucion/src/main/java/com/example/demo/Usuario.java
+++ evolucion/src/main/java/com/example/demo/Usuario.java
@@ -12,13 +12,24 @@ class Usuario {
 
     private @Id @GeneratedValue Long id;
     private String nombre;
+    private String apellido;
     private String cargo;
 
     Usuario() {}
 
-    Usuario(String nombre, String cargo) {
+    Usuario(String nombre,String apellido, String cargo) {
         this.nombre = nombre;
+        this.apellido = apellido;
         this.cargo = cargo;
+    }
+
+    public String getNombre(){
+        return this.nombre +" " + this.apellido;
+    }
+    public void setNombre(String name){
+        String[] partes = name.split(" ");
+        this.nombre = partes[0];
+        this.apellido = partes[1];
     }
 
 }
