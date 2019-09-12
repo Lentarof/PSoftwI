289d028d0aff23cdf4745ed4b23fc181be539fef
Ray Silva <onikageriki@gmail.com>
2019-09-12 03:11:21 -0400

    Agregando CargarDatos al directorio links

---
+++ links/src/main/java/com/example/demo/CargarDatos.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class CargarDatos {
+
+    @Bean
+    CommandLineRunner initDatabase(UsuarioRepositorio repositorio) {
+        return args -> {
+            log.info("Precarga " + repositorio.save(new Usuario("Romulo","Teran", "Gerente de MNE")));
+            log.info("Precarga " + repositorio.save(new Usuario("Abel","Tenaz", "Gerente de MNA")));
+        };
+    }
+}
