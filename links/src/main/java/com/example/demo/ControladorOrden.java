5def9a40008def4dd5462004111f0280de0dcd0f
Ray Silva <onikageriki@gmail.com>
2019-09-12 03:50:18 -0400

    Agregando ControladorOrden al directorio links

---
+++ links/src/main/java/com/example/demo/ControladorOrden.java
@@ -0,0 +1,63 @@
+package com.example.demo;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceSupport;
+import org.springframework.hateoas.Resources;
+import org.springframework.hateoas.VndErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControladorOrden {
+
+    private final OrdenRepositorio ordenRepositorio;
+    private final OrdenRecursoAssembler assembler;
+
+    ControladorOrden(OrdenRepositorio orderRepository,
+                    OrdenRecursoAssembler assembler) {
+
+        this.ordenRepositorio = ordenRepositorio;
+        this.assembler = assembler;
+    }
+
+    @GetMapping("/ordenes")
+    Resources<Resource<Orden>> all() {
+
+        List<Resource<Orden>> ordenes = ordenRepositorio.findAll().stream()
+                .map(assembler::toResource)
+                .collect(Collectors.toList());
+
+        return new Resources<>(ordenes,
+                linkTo(methodOn(ControladorOrden.class).all()).withSelfRel());
+    }
+
+    @GetMapping("/ordenes/{id}")
+    Resource<Orden> one(@PathVariable Long id) {
+        return assembler.toResource(
+                ordenRepositorio.findById(id)
+                        .orElseThrow(() -> new ExcepcionOrdenNoEncontrada(id)));
+    }
+
+    @PostMapping("/ordenes")
+    ResponseEntity<Resource<Orden>> nuevaOrden(@RequestBody Orden orden) {
+
+        orden.setEstado(Estado.IN_PROGRESS);
+        Orden nuevaOrden = ordenRepositorio.save(orden);
+
+        return ResponseEntity
+                .created(linkTo(methodOn(ControladorOrden.class).one(nuevaOrden.getId())).toUri())
+                .body(assembler.toResource(nuevaOrden));
+    }
+}
