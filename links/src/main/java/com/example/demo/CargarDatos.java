23a6627766a739bd77469280f36aa68a192a8dd9
Ray Silva <onikageriki@gmail.com>
2019-09-12 16:05:01 -0400

    Corrigiendo CargarDatos con repositorio de usuarios del directorio links

--- links/src/main/java/com/example/demo/CargarDatos.java
+++ links/src/main/java/com/example/demo/CargarDatos.java
@@ -12,11 +12,19 @@ import org.springframework.context.annotation.Configuration;
 class CargarDatos {
 
     @Bean
-    CommandLineRunner initDatabase(UsuarioRepositorio repositorio, OrdenRepositorio ordenRepositorio) {
+    CommandLineRunner initDatabase(UsuarioRepositorio usuarioRepositorio, OrdenRepositorio ordenRepositorio) {
         return args -> {
+
+            usuarioRepositorio.save(new Usuario("Romulo", "Teran", "MNE"));
+            usuarioRepositorio.save(new Usuario("Abel", "Tenaz", "MNA"));
+
+            usuarioRepositorio.findAll().forEach(usuario -> {
+                log.info("Precarga " + usuario);
+            });
+            /*  Anterior forma de cargar al repositorio usuarios
             log.info("Precarga " + repositorio.save(new Usuario("Romulo","Teran", "Gerente de MNE")));
             log.info("Precarga " + repositorio.save(new Usuario("Abel","Tenaz", "Gerente de MNA")));
-
+            */
             ordenRepositorio.save(new Orden("MacBook Pro", Estado.COMPLETED));
             ordenRepositorio.save(new Orden("iPhone", Estado.IN_PROGRESS));
 
